186bc76320b3be22ed161c9f4ce5c8e6db6ffc52
Rukhsana Kausar <rukhsana00801@gmail>
2026-08-12 16:32:15 +0500

    dom

---
+++ index.js
@@ -0,0 +1,18 @@
+document.write("Hello, world!");
+
+const subRef = document.querySelector
+("#subtitle");
+
+subRef.innerHTML = "Document Object Model (DOM) - web APIs - MDN Web DOCS";
+
+const paragraph= document.querySelector
+("#description");
+paragraph.innerHTML = " hi,My name is Rukhsana. i want to become development";
+
+const ageRef= document.querySelector
+("#age")
+console.log(ageRef.innerHTML);
+
+const age=Number(ageRef.innerHTML)
+console.log(typeof age)
+
